ee72220d4bbf96e3158f1b7c881839d3ea049013
BATMAN <cpwest11@gmail.com>
2013-03-26 13:00:54 -0500

    lol
    lol

--- snake.rkt
+++ snake.rkt
@@ -4,5 +4,25 @@
 (require 2htdp/image)
 (require 2htdp/universe)
 (define SCALE 10)
-(define segment (circle 10 "solid" "red"))
-(define SCENE (place-image (rectangle 1 200 "solid" "yellow") 199 100 (place-image (square 400 "solid" "black") 0 0 (empty-scene 200 200))))
+(define SCENE  (place-image (square 400 "solid" "black") 204 204 (place-image (square 410 "solid" "red") 204 204 (empty-scene 408 408))))
+(define SEGMENT (circle 10 "solid" "red"))
+(define FOOD (circle 10 "solid" "blue"))
+(define-struct world (worm food score))
+(define-struct worm (head segments))
+(define-struct head (posn dir))
+(define-struct segment (pos id))
+
+(define (controller w cmd)
+  (let* ([x (posn-x (head-posn (worm-head (world-worm w))))]
+         [y (posn-y (head-posn (worm-head (world-worm w))))]
+         [dir (head-dir (worm-head (world-worm w)))]
+         [head (worm-head (world-worm w))]
+         [worm (world-worm w)]
+         )
+    (cond
+      [(key=? cmd "w") (make-world (make-worm (make-head (head-posn head) "u") (worm-segments worm)) "food" "score")]
+      [(key=? cmd "a") (make-world (make-worm (make-head (head-posn head) "l") (worm-segments worm)) "food" "score")]
+      [(key=? cmd "s") (make-world (make-worm (make-head (head-posn head) "d") (worm-segments worm)) "food" "score")]
+      [(key=? cmd "d") (make-world (make-worm (make-head (head-posn head) "r") (worm-segments worm)) "food" "score")])))
+
+
